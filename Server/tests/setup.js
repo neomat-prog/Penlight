b306d15d133c02be6aa9d98dd3c3df72789c0e18
neomat-prog <thegreyliongaming@gmail.com>
2025-03-01 22:42:32 +0100

    Implement testing for registering a user

---
+++ Server/tests/setup.js
@@ -0,0 +1,14 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+let mongod;
+
+beforeAll(async () => {
+  mongod = await MongoMemoryServer.create();
+  const uri = mongod.getUri();
+  await mongoose.connect(uri);
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongod.stop();
+});
